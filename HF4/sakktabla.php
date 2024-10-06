4ac9cb9f53253b8acb72062b1646a88b425bc953
unknown <albertjszabolcs@uni.sapientia.ro>
2024-10-06 13:31:10 +0300

    HF4

---
+++ HF4/sakktabla.php
@@ -0,0 +1,21 @@
+<?php
+echo <<<EOD
+<table border="1" style="width: 100px; height: 100px; text-align: center;">
+    <tr>
+        <td>♜</td>
+        <td>♞</td>
+        <td>♝</td>
+    </tr>
+    <tr>
+        <td>♟</td>
+        <td>♟</td>
+        <td>♟</td>
+    </tr>
+    <tr>
+        <td></td>
+        <td></td>
+        <td></td>
+    </tr>
+</table>
+EOD;
+?>
